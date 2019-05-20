a7cca5f786dbb98e2990bdf534e7ebb149f8f0b3
freinold <reinold@hm.edu>
2019-05-21 00:22:58 +0200

    Layout modifications, added bootstrap and tried a little with pug

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+router.get("/insert_resource", (req, res, next) => {
+  res.render("insert_resource", { title: "Insert resource"});
 });
 
 module.exports = router;
