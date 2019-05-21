af61294e6a20b1e35357eff7f331e4fa2e64e639
freinold <reinold@hm.edu>
2019-05-21 11:53:48 +0200

    Changed from CSS to SASS. Added page for inserting row, changed layout template.

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SmartHome-REST' });
 });
 
 router.get("/insert_resource", (req, res, next) => {
-  res.render("insert_resource", { title: "Insert resource"});
+  res.render("insert_resource", { title: "Insert new resource"});
+});
+
+router.get("/insert_row", (req, res, next) => {
+  res.render("insert_row", { title: "Insert new row into resource"})
 });
 
 module.exports = router;
